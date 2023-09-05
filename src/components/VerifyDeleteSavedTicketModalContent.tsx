d1d69cba13e0616a9431a586a8ce839d77b04033
Sahin MARAL <sahin.maral@hotmail.com>
2023-09-05 03:04:52 +0300

    Removed unused imports

--- src/components/VerifyDeleteSavedTicketModalContent.tsx
+++ src/components/VerifyDeleteSavedTicketModalContent.tsx
@@ -1,7 +1,7 @@
 import { FC, MouseEvent } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faClose } from "@fortawesome/free-solid-svg-icons";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { setModalVisibilityToggle } from "../redux/app/appSlice";
 import { useToaster } from "../context/ToasterProvider";
 import BoughtTicketType from "../types/BoughtTicketType";
